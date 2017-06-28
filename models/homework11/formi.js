30669a1819f97efdae2f29799863b20abea9fbd8
shivam <coolshivam686@gmail.com>
2017-06-28 11:07:34 +0530

    shivam project

---
+++ models/homework11/formi.js
@@ -0,0 +1,32 @@
+/**
+ * Created by XYZ on 21-06-2017.
+ */
+window.onload = function () {
+
+    // var input = document.getElementById('input');
+    // // console.log(a);
+    //
+    // var  = document.getElementById('hi');
+    //
+    // var c = document.getElementById('value');
+    //
+    // var d = document.getElementById('vhi');
+
+    var f = document.getElementById('form');
+    var m=document.getElementById('nam');
+
+
+
+    
+
+
+}
+
+
+function validateForm() {
+    var x = document.forms["formii"]["inp"].value;
+    if (x == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+}
